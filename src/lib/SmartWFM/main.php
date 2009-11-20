9d3e4de097b508f9ce96bf7fc68fb8eef4dee377
Philipp Seidel <phse@users.sourceforge.net>
2009-11-20 13:17:50 +0100

    Adding Copying informations to the source files

--- src/lib/SmartWFM/main.php
+++ src/lib/SmartWFM/main.php
@@ -1,4 +1,14 @@
 <?php
+###############################################################################
+# This file is a part of the SmartWFM PHP-Backend                             #
+# Copyright (C) 2009 Philipp Seidel <phibo@oss.dinotools.de>                  #
+#                                                                             #
+# SmartWFM PHP-Backend is free software; you can redestribute it and/or modify#
+# it under terms of GNU General Public License by Free Software Foundation.   #
+#                                                                             #
+# This program is distributed in the hope that it will be useful, but         #
+# WITHOUT ANY WARRANTY. See GPLv3 for more details.                           #
+###############################################################################
 
 //emulate json_decode and json_encode functions for PHP < 5.2
 
