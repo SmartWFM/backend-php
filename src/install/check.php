7b42e51b53457530edff84dc190187913a34854d
Morris Jobke <kabum@users.sourceforge.net>
2010-08-31 14:06:42 +0200

    save current state - install

--- src/install/check.php
+++ src/install/check.php
@@ -10,13 +10,53 @@
 # WITHOUT ANY WARRANTY. See GPLv3 for more details.                           #
 ###############################################################################
 
-#$params = $_GET['params'];
-?>
-<pre>
-<?php
-print_r(get_loaded_extensions());
-?>
-</pre>
-<?php
-phpinfo(INFO_MODULES);
+ini_set('display_errors',1);
+error_reporting(E_ALL|E_STRICT);
+
+$CONFIG_PATH = '../config/';
+$CONFIG_FILE = 'local.php';
+
+
+
+$check = intval($_GET['check']);
+
+$result = array();
+$result['error'] = false;
+switch($check){
+	case 1: # check directory and files
+		$result['result']['config'] = array(
+			'base_path' => '/var/www',
+		
+		);
+		$result['result']['writable'] = false;
+		$result['result']['overwrite'] = false;
+		if(file_exists($CONFIG_PATH)) {
+			if(is_writable($CONFIG_PATH))
+				$result['result']['writable'] = true;
+		}
+		if(file_exists($CONFIG_PATH.$CONFIG_FILE)) {
+			$result['result']['overwrite'] = true;
+			//$result['result']['config'] = NULL; //TODO read config
+		}		
+		break;
+	case 2:
+		$path = @$_GET['path'];
+		if($path == '')
+			$result['error'] = true;
+		else {
+			if(!file_exists($path))
+				$result['result']['correct'] = false;
+			else
+				$result['result']['correct'] = true;			
+		}
+		break;
+	default:
+		$result['error'] = true;
+		break;
+
+}
+
+
+header("Content-Type: application/json");
+print json_encode($result);
 ?>
--- src/install/index2.php
+++ src/install/index2.php
@@ -9,49 +9,6 @@
 # This program is distributed in the hope that it will be useful, but         #
 # WITHOUT ANY WARRANTY. See GPLv3 for more details.                           #
 ###############################################################################
-
-ini_set('display_errors',1);
-error_reporting(E_ALL|E_STRICT);
-
-$CONFIG_PATH = '../config/';
-$CONFIG_FILE = 'local.php';
-
-
-# SETTINGS
-
-$COMMANDS_PATH = 'commands/';
-$BASE_PATH = '/var/www';
-
-$writable = false;
-$overwrite = false;
-if(file_exists($CONFIG_PATH)) {
-	if(is_writable($CONFIG_PATH))
-		$writable = true;
-}
-
-if(file_exists($CONFIG_PATH.$CONFIG_FILE)) {
-	//TODO read config
-	$overwrite = true;
-}
-
-	
-$commands = array();
-
-$h = @opendir('../'.$COMMANDS_PATH);
-if(is_resource($h)) {
-	while( ($f = readdir($h)) !== false ) {
-		if(preg_match('!^\.{1,2}$!', $f))
-			continue;
-		if(preg_match('!.*~$!', $f))
-			continue;
-		if(!is_dir('../'.$COMMANDS_PATH.$f)) {
-			if(strlen($f) >= 4 and substr($f, -4) == '.php') {
-				$commands[] = substr($f,0,-4);
-			}
-		}
-	}
-	closedir($h);
-}
 ?>
 <!DOCTYPE html 
      PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -61,70 +18,89 @@ if(is_resource($h)) {
 		<meta http-equiv="content-type" content="application/xhtml+xml;charset=utf-8" />
 		<title>Installation SWFM - PHP Backend</title>		
 		<script type="text/javascript" src="jquery.js"></script>
-		<style type="text/css"><!--
-			.notice {
-				padding:5px;
-				border:2px solid #009900;
-				background-color:#CCFFCC;
-				text-align:center;
-				margin:20px 50px;
-			}
-			.error {
-				padding:5px;
-				border:2px solid #CC0000;
-				background-color: #FFCCCC;
-				text-align:center;
-				margin:20px 50px;
-			}
-			body {
-				text-align: center;
-			}
-			#settings {
-				margin: 0px 50px;
-				text-align: left;
-			}
-			#wrapper {
-				width: 700px;
-			}
-		//--></style>
+		<link rel="stylesheet" media="all" href="style.css" />
+		<script type="text/javascript">
+		<!--		
+		function addNotice(text) {
+			$('#notify').append('<div class="notice">'+text+'</div>');
+		}
+		
+		function addError(text) {
+			$('#notify').append('<div class="error">'+text+'</div>');
+		}
+		
+		function setCorrectFlag(id){
+			$('#'+id).attr('src', 'images/correct.png');
+		}
+		
+		function setFalseFlag(id){
+			$('#'+id).attr('src', 'images/false.png');
+		}
+		
+		function checkConfigDir() {
+			$.ajax({
+				url: 'check.php',
+				data: {'check': 1},
+				success: function(data) {
+					if(data.error == false){
+						if(data.result.writable)
+							addNotice('Directory is writable');
+						else
+							addError('Directory is not writable');
+						if(data.result.overwrite)
+							addError('Config file exists');
+						else
+							addNotice('Config file doesn\'t exists');
+						// load data
+						$('input[name="basepath"]').val(data.result.config.base_path);
+						checkBasePath();
+					}
+				}
+			});
+		}
+		
+		function init() {
+			checkConfigDir();
+			$('#loading').hide();
+		}
+		
+		function checkBasePath() {
+			$.ajax({
+				url: 'check.php',
+				data: {'check': 2, 'path': $('input[name="basepath"]').val()},
+				success: function(data) {
+					if(data.error == false){
+						if(data.result.correct)
+							setCorrectFlag('basepath-check');
+						else
+							setFalseFlag('basepath-check');
+						
+					}
+					else
+						setFalseFlag('basepath-check');
+				}
+			});
+		}
+		//-->
+		</script>
 	</head>
-<body>
+<body onload="init()">
 	<center>
 	<div id="wrapper">
 	
 	<h1>Installation SWFM - PHP Backend</h1>
+	<img src="images/ajax-loader.gif" title="Loading..." id="loading"/>
+	<div id="notify"></div>
 	
-	<div id="notify">
-	<?php
-		if($writable) {
-			?>
-			<div class="notice">Directory is writable</div>
-			<?php
-		} else {
-			?>
-			<div class="error">Directory is not writable</div>
-			<?php		
-		}	
-		if($overwrite) {
-			?>
-			<div class="error">Config file exists</div>
-			<?php
-		} else {
-			?>
-			<div class="notice">Config file doesn't exists</div>
-			<?php		
-		}	
-	?>
-	</div>
 	
 	<div id="settings">
 		<form action="save.php" method="post">
-			<p>
+			<p id="basepath">
 				<label for="basepath">basepath of SWFM installation - access only under this directory</label><br />
-				<input name="basepath" type="text" size="50" value="<?php echo $BASE_PATH; ?>" />
-				<img id="basepath-check" src="correct.png"/>
+				<input name="basepath" type="text" size="50" onchange="checkBasePath()" />
+				<img id="basepath-check" src="images/false.png"/>
 			</p>
-			<p>
+			<!--<p>
 				<label for="commandspath">path to commands directory</label><br />
 				<input name="commandspath" type="text" size="50" value="<?php echo $COMMANDS_PATH; ?>" >
 			</p>
@@ -136,7 +112,7 @@ if(is_resource($h)) {
 				}
 				
 				?>
-			</p>
+			</p>-->
 			<p>
 				<label for="mimetype_detection_mode">mimetype_detection_mode</label><br />
 				<select name="mimetype_detection_mode" size="1">
@@ -146,23 +122,23 @@ if(is_resource($h)) {
 				</select>
 			</p>
 			<p>
-				<label for="filesystem_type">filesystem_type</label><br />
+				<label for="filesystem_type">type of filesystem</label><br />
 				<select name="filesystem_type" size="1">
 					<option value="local">local</option>
 					<option value="afs">afs</option>
 				</select>
 			</p>
-			<p>
+			<!--<p>
 				<label for="use_x_sendfile">use_x_sendfile</label><br />
 				<input name="use_x_sendfile" type="checkbox" value="true" />
 			</p>
-			<input type="submit" value="Submit" />
+			<input type="submit" value="Submit" />-->
 		
 		</form>
 	</div>
 	
-	</div>
 	
+	</div>
 	</center>
 </body>
 </html>
