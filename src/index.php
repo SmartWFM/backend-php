a94cdc444128aa8bff6ebc998ae05dc7bc329b38
Morris Jobke <morris.jobke@gmail.com>
2013-02-04 14:14:23 +0100

    fix umlauts bug

--- src/index.php
+++ src/index.php
@@ -17,6 +17,20 @@ if(file_exists('install') && !file_exists('config/local.php')) {
 	exit(0);
 }
 
+/**
+ * source: http://trac.phtagr.org/attachment/ticket/83/patch-broken-escapeshellarg.diff
+ *
+ * In some php versions escapeshellarg() removes special characters like german
+ * umlaut. This bug causes wrong system calls to files like import of media
+ * with umlaut. The call setlocale() to an UTF8 character set fixes this
+ * problem. Check your character sets of our environment with "local -a" and
+ * enter it below.
+ *
+ * See also http://bugs.php.net/bug.php?id=44945
+ *          http://bugs.php.net/bug.php?id=44564
+ */
+setlocale(LC_CTYPE, 'UTF8', 'de_DE.UTF-8');
+
 define('SMARTWFM_DEBUG', false);
 
 require_once("lib/FirePHPCore/fb.php");
