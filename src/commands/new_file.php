cd39e55dd277b36277b4c99edbbaa074714f93fd
Morris Jobke <morris.jobke@gmail.com>
2013-03-11 13:40:25 +0100

    added save command

--- src/commands/new_file.php
+++ src/commands/new_file.php
@@ -87,11 +87,11 @@ class NewFile_Create extends SmartWFM_Command {
 		if($fs_type == 'afs') {
 			$afs = new afs($root_path);
 			if(!$afs->allowed(AFS_CREATE)) {
-				throw new SmartWFM_Exception('Permission denied.', -9);
+				throw new SmartWFM_Exception('Permission denied', -9);
 			}
 		} else if ($fs_type == 'local') {
 			if(!is_writable($root_path)) {
-				throw new SmartWFM_Exception('Permission denied.', -9);
+				throw new SmartWFM_Exception('Permission denied', -9);
 			}
 		}
 		// ToDo: check if file exists
@@ -115,6 +115,72 @@ class NewFile_Create extends SmartWFM_Command {
 }
 
 SmartWFM_CommandManager::register('new_file.create', new NewFile_Create());
+
+
+class NewFile_Save extends SmartWFM_Command {
+	function process($params) {
+		$fs_type = SmartWFM_Registry::get('filesystem_type');
+
+		$base_path = SmartWFM_Registry::get('basepath','/');
+
+		// check params
+		$param_test = new SmartWFM_Param(
+			$type = 'object',
+			$items = array(
+				'path' => new SmartWFM_Param('string'),
+				'name' => new SmartWFM_Param('string'),
+				'content' => new SmartWFM_Param('string')
+			)
+		);
+
+		$params = $param_test->validate($params);
+
+		// join path
+		$root_path = Path::join(
+			$base_path,
+			$params['path']
+		);
+
+		$filename = Path::join(
+			$root_path,
+			$params['name']
+		);
+
+		// validate path
+		if(Path::validate($base_path, $root_path) != true || Path::validate($base_path, $filename) != true) {
+			throw new SmartWFM_Exception('Wrong filename');
+		}
+
+		// check some stuff
+		if($fs_type == 'afs') {
+			$afs = new afs($root_path);
+			if(!$afs->allowed(AFS_INSERT)) {
+				throw new SmartWFM_Exception('Permission denied', -9);
+			}
+		} else if ($fs_type == 'local') {
+			if(!is_writable($filename)) {
+				throw new SmartWFM_Exception('Permission denied', -9);
+			}
+		}
+		// ToDo: check if file exists
+
+		if(!$handle = fopen($filename, 'w')) {
+			throw new SmartWFM_Exception('Couldn\'t open the file', -1);
+		}
+
+		if(!fwrite($handle, $params['content'])) {
+			throw new SmartWFM_Exception('Couldn\'t write to file', -2);
+		}
+
+		fclose($handle);
+
+		$response = new SmartWFM_Response();
+		$response->data = True;
+		return $response;
+	}
+}
+
+SmartWFM_CommandManager::register('new_file.save', new NewFile_Save());
 
 ?>
 
